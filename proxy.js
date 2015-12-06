5dbee91b1f49e675b9b6daa8e50ceb803c1f9fc9
David García Sánchez <dgarcia@aurigae.com>
2015-12-06 21:05:23 +0100

    Create proxy server

---
+++ proxy.js
@@ -0,0 +1,30 @@
+var util = require('util');
+var http = require('http');
+var url = require('url');
+var PORT = 8080;
+var feed = '';
+
+http.createServer(function(request, response) {
+  request = url.parse(request.url, true);
+  if(!request.query.feed || !request.query.callback) {
+    response.end();
+  }
+  else {
+    http.get(request.query.feed, function(respFeed) {
+      respFeed.on('data', function(data) {
+        feed += data;
+      });
+      respFeed.on('end', function(data) {
+        console.log("Feed fetched from " + request.query.feed);
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.writeHead(200, {'Content-Type': 'text/xml'});
+        //response.end(request.query.callback + "('" + feed + "')"); // JSONP
+        response.end(feed);
+      });
+    });
+    feed = '';
+  }
+}).listen(process.env.PORT || PORT);
+
+console.log('Server listening on port ' + PORT);
